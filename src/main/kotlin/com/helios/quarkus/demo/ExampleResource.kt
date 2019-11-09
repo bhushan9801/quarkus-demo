2d5cfd400303541f41ef46a55467d11a488dc899
Bhushan Rane <ekpravasi@gmail.com>
2019-11-09 19:45:44 +0530

    Datasource use

--- src/main/kotlin/com/helios/quarkus/demo/ExampleResource.kt
+++ src/main/kotlin/com/helios/quarkus/demo/ExampleResource.kt
@@ -1,14 +1,20 @@
 package com.helios.quarkus.demo
 
+import io.agroal.api.AgroalDataSource
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 @Path("/hello")
-class ExampleResource {
+class ExampleResource(val dataSource: AgroalDataSource) {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "hello"
+    fun hello(): String {
+        val statement = dataSource.connection.createStatement()
+        statement.executeQuery("select * from dual")
+        return "hello"
+    }
 }
