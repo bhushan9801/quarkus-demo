369bb8909de70437779d978716dc6580b5df51ce
Bhushan Rane <ekpravasi@gmail.com>
2019-11-17 22:10:34 +0530

    Enable editor config for projects

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,72 +1,72 @@
 <template>
   <div id="app" class="container">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img alt="Vue logo" src="./assets/logo.png"/>
     <films-table v-bind:films="films"></films-table>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import FilmsTable from "./components/FilmsTable.vue";
+    // import HelloWorld from './components/HelloWorld.vue'
+    import FilmsTable from "./components/FilmsTable.vue";
 
-export default {
-  name: "app",
-  components: {
-    FilmsTable
-  },
-  data() {
-    return {
-      employees: [
-        {
-          id: 1,
-          name: "Richard Hendricks",
-          email: "richard@piedpiper.com"
+    export default {
+        name: "app",
+        components: {
+            FilmsTable
         },
-        {
-          id: 2,
-          name: "Bertram Gilfoyle",
-          email: "gilfoyle@piedpiper.com"
+        data() {
+            return {
+                employees: [
+                    {
+                        id: 1,
+                        name: "Richard Hendricks",
+                        email: "richard@piedpiper.com"
+                    },
+                    {
+                        id: 2,
+                        name: "Bertram Gilfoyle",
+                        email: "gilfoyle@piedpiper.com"
+                    },
+                    {
+                        id: 3,
+                        name: "Dinesh Chugtai",
+                        email: "dinesh@piedpiper.com"
+                    }
+                ],
+                films: []
+            };
         },
-        {
-          id: 3,
-          name: "Dinesh Chugtai",
-          email: "dinesh@piedpiper.com"
+        methods: {
+            async getFilms() {
+                try {
+                    const films = await fetch("http://localhost:8080/api/films?order=filmId", {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Accept: "application/json"
+                        }
+                    });
+                    const data = await films.json();
+                    this.films = data;
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+        mounted() {
+            console.log("mounted called");
+            this.getFilms();
         }
-      ],
-      films: []
     };
-  },
-  methods: {
-    async getFilms() {
-      try {
-        const films = await fetch("http://localhost:8080/api/films?order=filmId", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          }
-        });
-        const data = await films.json();
-        this.films = data;
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  },
-  mounted() {
-    console.log("mounted called");
-    this.getFilms();
-  }
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- vue-app/src/components/FilmsTable.vue
+++ vue-app/src/components/FilmsTable.vue
@@ -2,30 +2,30 @@
   <div id="films-table">
     <table class="table">
       <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-        </tr>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+      </tr>
       </thead>
       <tbody>
-        <tr v-for="f in films" :key="f.filmId">
-          <td>{{f.filmId}}</td>
-          <td>{{f.title}}</td>
-          <td class="text-md-left">{{f.description}}</td>
-        </tr>
+      <tr v-for="f in films" :key="f.filmId">
+        <td>{{f.filmId}}</td>
+        <td>{{f.title}}</td>
+        <td class="text-md-left">{{f.description}}</td>
+      </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-export default {
-  name: "films-table",
-  props: {
-    films: Array
-  }
-};
+    export default {
+        name: "films-table",
+        props: {
+            films: Array
+        }
+    };
 </script>
 
 <style scoped></style>
